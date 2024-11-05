05f0ce75687b0d6218f09b6c689cd58848b0c443
Ioannis Anifantakis <ianyfantakis@deloitte.gr>
2024-11-05 13:11:12 +0200

    Added missing drawable to the Icon object

--- app/src/main/java/eu/anifantakis/snoozeloo/core/presentation/designsystem/Icons.kt
+++ app/src/main/java/eu/anifantakis/snoozeloo/core/presentation/designsystem/Icons.kt
@@ -1,5 +1,7 @@
 package eu.anifantakis.snoozeloo.core.presentation.designsystem
 
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Add
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.vectorResource
@@ -7,8 +9,15 @@ import eu.anifantakis.snoozeloo.R
 
 object Icons {
 
+    // icon definition via stored resources
     val alarm: ImageVector
         @Composable
         get() = ImageVector.vectorResource(id = R.drawable.alarm)
+
+
+    // icon via material icons library
+    val add: ImageVector
+        @Composable
+        get() = Icons.Outlined.Add
 
 }
